12776bb37edfeea578ff6d424538daa618168636
SixHao <15194953567@163.com>
2017-12-16 19:09:25 +0800

    123

--- resources/views/Home/shopcart2.blade.php
+++ resources/views/Home/shopcart2.blade.php
@@ -1,67 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+@extends('layout/Hlayout')
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-	<title>购物车页面</title>
-
-	<link href="{{asset('Home/AmazeUI-2.4.2/assets/css/amazeui.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('Home/basic/css/demo.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('Home/css/cartstyle.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('Home/css/optstyle.css')}}" rel="stylesheet" type="text/css" />
-	{{--<script src="{{asset('Admin/assets/js/jquery.js')}}"></script>--}}
-	<script type="text/javascript" src="{{asset('Home/js/jquery.js')}}"></script>
-	<script src="{{asset('Admin/assets/js/ch-ui.admin.js')}}"></script>
-	<script type="text/javascript" src="{{asset('layer/layer.js')}}"></script>
-</head>
-
-<body>
-
-<!--顶部导航条 -->
-<div class="am-container header">
-	<ul class="message-l">
-		<div class="topMessage">
-			<div class="menu-hd">
-				<a href="./login" target="_top" class="h">亲，请登录</a>
-				<a href="./zhuce" target="_top">免费注册</a>
-			</div>
-		</div>
-	</ul>
-	<ul class="message-r">
-		<div class="topMessage home">
-			<div class="menu-hd"><a href="index" target="_top" class="h">商城首页</a></div>
-		</div>
-		<div class="topMessage my-shangcheng">
-			<div class="menu-hd MyShangcheng"><a href="myinfo" target="_top"><i class="am-icon-user am-icon-fw"></i>个人中心</a></div>
-		</div>
-		<div class="topMessage mini-cart">
-			<div class="menu-hd"><a id="mc-menu-hd" href="shopcart" target="_top"><i class="am-icon-shopping-cart  am-icon-fw"></i><span>购物车</span><strong id="J_MiniCartNum" class="h">0</strong></a></div>
-		</div>
-		<div class="topMessage favorite">
-			<div class="menu-hd"><a href="#" target="_top"><i class="am-icon-heart am-icon-fw"></i><span>收藏夹</span></a></div>
-	</ul>
-</div>
-
-<!--悬浮搜索框-->
-
-<div class="nav white">
-	<div class="logo"><img src="{{asset('Home/images/logo.png')}}" /></div>
-	<div class="logoBig">
-		<li><img src="{{asset('Home/images/logobig.png')}}" /></li>
-	</div>
-
-	<div class="search-bar pr">
-		<a name="index_none_header_sysc" href="#"></a>
-		<form action="home/">
-			<input id="searchInput" name="index_none_header_sysc" type="text" placeholder="搜索" autocomplete="off">
-			<input id="ai-topsearch" class="submit am-btn" value="搜索" index="1" type="submit">
-		</form>
-	</div>
-</div>
-
-<div class="clear"></div>
+@section('title')
+	<title>网站首页</title>
+@endsection
+<link href="{{asset('Home/AmazeUI-2.4.2/assets/css/amazeui.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('Home/basic/css/demo.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('Home/css/cartstyle.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('Home/css/optstyle.css')}}" rel="stylesheet" type="text/css" />
+{{--<script src="{{asset('Admin/assets/js/jquery.js')}}"></script>--}}
+<script type="text/javascript" src="{{asset('Home/js/jquery.js')}}"></script>
+<script src="{{asset('Admin/assets/js/ch-ui.admin.js')}}"></script>
+<script type="text/javascript" src="{{asset('layer/layer.js')}}"></script>
+@section('content')
 
 <!--购物车 -->
 <div class="concent">
@@ -93,6 +43,8 @@
 		<div class="clear"></div>
 		<div style="height: 100px; text-align: center; font-size: 18px;">
 		您的购物车为空！！！ 先去逛一逛再来吧！！！
+			<br>
+			<a style="padding: 10px;display: block; width: 100px; margin:0 auto; margin-top:20px;background-color:#ee3495; color: #fff; border-radius: 5px;" href="{{ url('/home/index') }}">返回首页</a>
 		</div>
 
 
@@ -140,46 +92,4 @@
 
 				</div>
 
-				<div class="footer">
-					<div class="footer-hd">
-						<p>
-							<a href="#">恒望科技</a>
-							<b>|</b>
-							<a href="#">商城首页</a>
-							<b>|</b>
-							<a href="#">支付宝</a>
-							<b>|</b>
-							<a href="#">物流</a>
-						</p>
-					</div>
-					<div class="footer-bd">
-						<p>
-							<a href="#">关于恒望</a>
-							<a href="#">合作伙伴</a>
-							<a href="#">联系我们</a>
-							<a href="#">网站地图</a>
-							<em>© 2015-2025 Hengwang.com 版权所有</em>
-						</p>
-					</div>
-				</div>
-
-	</div>
-
-
-
-
-	<!--引导 -->
-	<div class="navCir">
-		<li><a href="{{asset('Home/home/home.html')}}"><i class="am-icon-home "></i>首页</a></li>
-		<li><a href="{{asset('Home/home/sort.html')}}"><i class="am-icon-list"></i>分类</a></li>
-		<li class="active"><a href="shopcart.blade.php"><i class="am-icon-shopping-basket"></i>购物车</a></li>
-		<li><a href="{{asset('Home/person/index.html')}}"><i class="am-icon-user"></i>我的</a></li>
-	</div>
-
-
-
-
-
-</body>
-
-</html>
+@stop
--- resources/views/layout/Hlayout.blade.php
+++ resources/views/layout/Hlayout.blade.php
@@ -45,7 +45,7 @@
                 <div class="menu-hd MyShangcheng"><a href="{{ url('/home/userinfo/index') }}" target="_top"><i class="am-icon-user am-icon-fw"></i>个人中心</a></div>
             </div>
             <div class="topMessage mini-cart">
-                <div class="menu-hd"><a id="mc-menu-hd" href="{{ url('/home/shopcart') }}" target="_top"><i class="am-icon-shopping-cart  am-icon-fw"></i><span>购物车</span><strong id="J_MiniCartNum" class="h">0</strong></a></div>
+                <div class="menu-hd"><a id="mc-menu-hd" href="{{ url('/home/cart') }}" target="_top"><i class="am-icon-shopping-cart  am-icon-fw"></i><span>购物车</span><strong id="J_MiniCartNum" class="h">0</strong></a></div>
             </div>
 
         </ul>
